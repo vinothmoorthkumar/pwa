7c1e2d664a6d2baddf5463c14af08db094b3fcac
Vinoth kumar <vinothmoorthy.kumar@gmail.com>
2020-05-08 19:20:54 +0530

    prod mode fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MatToolbarModule, MatMenuModule, MatCardModule, MatIconModule, MatButtonModule, MatChipsModule,
-  MatFormFieldModule } from  '@angular/material';
+  MatFormFieldModule, MatAutocompleteModule } from  '@angular/material';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { HomeComponent } from './home/home.component';
 import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 import { SettingComponent } from './setting/setting.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,9 @@ import { SettingComponent } from './setting/setting.component';
     MatMenuModule,
     MatChipsModule,
     MatFormFieldModule,
+    FormsModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
